4d8c2228445dc4136bf709b3dbac7414fe1a36c9
Daniel Calderón <daniel.calderon@gokatch.com>
2022-08-18 20:25:58 +0200

    Update design

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Header from "./header"
 
@@ -35,15 +36,57 @@ const Layout = ({ children }) => {
           {children}
         </main>
 
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          &copy; {new Date().getFullYear()} &middot; Eso es como todo
+        <footer className="footer">
+          <nav className="footer__listenUs">
+            <h1 className="footer__title">Escuchanos en</h1>
+            <ul className="footer__links">
+              <li>
+                <a href="https://www.ivoox.com/escuchar-eso-es-como-todo_nq_1350840_1.html"
+                  rel="me"
+                  title="Eso es como todo en iVoox"
+                  aria-label="Eso es como todo en iVoox">
+                  <StaticImage
+                    src="../images/ivoox.png"
+                    formats={["auto", "webp", "png"]}
+                    alt="Eso es como todo en iVoox" />
+                </a>
+              </li>
+              <li>
+                <a href="https://open.spotify.com/show/7KdIzvC8bKAD4MCo2dlu2O"
+                  rel="me"
+                  title="Eso es como todo en Spotify"
+                  aria-label="Eso es como todo en Spotify">
+                  <StaticImage
+                    src="../images/spotify.png"
+                    formats={["auto", "webp", "png"]}
+                    alt="Eso es como todo en Spotify" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com/channel/UCvL-YC95Oh7Gu-WrOynq3zA"
+                  rel="me"
+                  title="Eso es como todo en YouTube"
+                  aria-label="Eso es como todo en YouTube">
+                  <StaticImage
+                    src="../images/youtube.png"
+                    formats={["auto", "webp", "png"]}
+                    alt="Eso es como todo en YouTube" />
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="footer__twitter">
+            <a href="https://twitter.com/EsoEsComoToders"
+              rel="me">
+              <span className="footer__twitterName">EsoEsComoToders</span>
+            </a>
+          </div>
         </footer>
       </div>
+      <p class="footer__copyright">
+        &copy; {new Date().getFullYear()} &middot; Eso es como todo
+      </p>
     </>
   )
 }
